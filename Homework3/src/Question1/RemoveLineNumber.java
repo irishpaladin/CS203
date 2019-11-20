881a3dc738ec5224a234bbaf7b1924ec5489166b
Stephanie Irish Paladin <stephanieirishpaladin>
2019-11-19 23:18:50 -0600

    Question1 done
    file I/O
    String manipulation using StringBuffer

---
+++ Homework3/src/Question1/RemoveLineNumber.java
@@ -0,0 +1,76 @@
+/**
+ * Program that prompts the user to enter a file name,
+ * reads the lines from the file, and replaces the line
+ * numbers in the file with appropriate numbers of spaces
+ *
+ * @course CS203
+ * @author Stephanie Irish Paladin 200413341 Assignment 3 last modified: 11/02
+ * 2pm
+ */
+package Question1;
+
+import java.util.Scanner;
+import java.io.File;
+import java.io.PrintWriter;
+
+
+public class RemoveLineNumber {
+
+    public static void main(String[] args) throws Exception {
+        // read the file input
+        Scanner inputFile = new Scanner(System.in);
+        boolean file_exist = false;
+        File file;
+
+        do {
+            //prompts the user to input the file path
+            System.out.println("Remove Line Number Program");
+            System.out.print("  Enter file path: ");
+            String filename = inputFile.nextLine();
+
+            // create file object
+            file = new File(filename);
+            if (file.exists()) {
+                file_exist = true;
+            } else {
+                System.out.println("\n  Error: File not found.\n");
+            }
+        } while (!file_exist);
+
+        // read the file
+        // stores the string that will be written
+        StringBuilder output_str = new StringBuilder("");
+        try ( Scanner read = new Scanner(file);) {
+            while (read.hasNext()) {
+                String str = read.nextLine();
+                StringBuilder str_builder = new StringBuilder(str);
+
+                // true if line has linenumber
+                if (str.matches("\\s*\\d+.*")) {
+                    for (int i = 0; i < str.length(); i++) {
+                        // stops the loop at the first instance of character
+                        if (Character.isAlphabetic(str.charAt(i))) {
+                            break;
+                        }
+                        // replaces the line number to space
+                        if (Character.isDigit(str.charAt(i))) {
+                            str_builder.setCharAt(i, ' ');
+                        }
+                    }
+                }// end of if
+                //adds endline after every concatanation of lines
+                output_str.append(str_builder.append("\n"));
+            }// end of while
+            // removes the extra endline at the end of the stringbuilder
+            output_str.deleteCharAt(output_str.length() - 1);
+        }
+
+        // write the file
+        try ( PrintWriter write = new PrintWriter(file);) {
+            write.print(output_str);
+            System.out.println("\n  Removed line number successfully.");
+        }
+
+    }
+
+}
---
+++ Homework3/src/Question2/Circle.java
@@ -0,0 +1,38 @@
+public class Circle extends GeometricObject {
+  private double radius;
+
+  public Circle() {
+  }
+
+  public Circle(double radius) {
+    this.radius = radius;
+  }
+
+  public Circle(double radius,
+      String color, boolean filled) {
+    this.radius = radius;
+    setColor(color);
+    setFilled(filled);
+  }
+
+  /** Return radius */
+  public double getRadius() {
+    return radius;
+  }
+
+  /** Set a new radius */
+  public void setRadius(double radius) {
+    this.radius = radius;
+  }
+
+  /** Return diameter */
+  public double getDiameter() {
+    return 2 * radius;
+  }
+
+  /* Print the circle info */
+  public void printCircle() {
+    System.out.println("The circle is created " + getDateCreated() +
+      " and the radius is " + radius);
+  }
+}
